c15a7212a8ee33263b998e2fb99020e41cc0a38f
Cryptoforge <cryptoforge.cc@gmail.com>
2026-01-18 20:05:06 -0800

    Set Qt::AA_EnableHighDpiScaling before starting Qt

--- src/qt/komodo.cpp
+++ src/qt/komodo.cpp
@@ -640,8 +640,6 @@ int main(int argc, char *argv[])
     Q_INIT_RESOURCE(komodo);
     Q_INIT_RESOURCE(komodo_locale);
 
-    QApplication::setStyle("fusion");
-    KomodoApplication app(argc, argv);
 #if QT_VERSION > 0x050100
     // Generate high-dpi pixmaps
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
@@ -649,6 +647,9 @@ int main(int argc, char *argv[])
 #if QT_VERSION >= 0x050600
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+
+    QApplication::setStyle("fusion");
+    KomodoApplication app(argc, argv);
 #ifdef Q_OS_MAC
     QApplication::setAttribute(Qt::AA_DontShowIconsInMenus);
 #endif
