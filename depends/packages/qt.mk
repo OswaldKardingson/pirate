b43821835160d94dd839e88599da377d675c8bc8
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2026-02-01 14:32:43 +0200

    Disable hardening on MacOS to avoid Relro checks & patch libpng to skip including fp.h which isn't supported on Mac

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -24,6 +24,7 @@ $(package)_patches += memory_resource.patch
 $(package)_patches += utc_from_string_no_optimize.patch
 $(package)_patches += windows_lto.patch
 $(package)_patches += zlib-timebits64.patch
+$(package)_patches += libpng-no-fp-h-darwin.patch
 
 $(package)_qttranslations_file_name=qttranslations-$($(package)_suffix)
 $(package)_qttranslations_sha256_hash=24d4c58bc2a40c0f44f59ee64af4192c7d0038c1e45af61646cfc5b65058f271
